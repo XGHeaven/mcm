45a42e0c0c855923ef28b1d51389d82ad497574b
XGHeaven <xgheaven@gmail.com>
2020-07-07 00:59:51 +0800

    fix: md5 dep 404

--- lib/deps.ts
+++ lib/deps.ts
@@ -5,7 +5,7 @@ export * as colors from "https://deno.land/std@0.59.0/fmt/colors.ts";
 export * as async from "https://deno.land/std@0.59.0/async/mod.ts";
 export * as asserts from "https://deno.land/std@0.59.0/testing/asserts.ts";
 
-export { md5 } from "https://deno.land/x/md5/mod.ts";
+export { md5 } from "https://denopkg.com/chiefbiiko/md5/mod.ts";
 
 export { hmac } from "https://denopkg.com/chiefbiiko/hmac/mod.ts";
 export { sha1 } from "https://denopkg.com/chiefbiiko/sha1/mod.ts";
